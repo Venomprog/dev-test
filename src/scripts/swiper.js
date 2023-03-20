2d14c91af3b006ff77ce608d012d43d385e9c8f6
Gleb <glebshaevvv@mail.ru>
2023-03-20 17:21:33 +0300

    update

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -1,172 +1,82 @@
 import Swiper from 'swiper/bundle'
 
-const swiper = new Swiper('.image-swiper', {
-    speed: 700,
-    spaceBetween: 100,
-    direction: 'horizontal',
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-});
+const sliderList = document.querySelectorAll('[data-slider]')
 
 
-const swiperHistory = new Swiper('.slider__wrapper--history', {
-    speed: 700,
-    spaceBetween: 100,
-    direction: 'horizontal',
-    // loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.right-slide',
-        prevEl: '.left-slide',
-    },
-    
-});
-
-
-
-const swiperThumbs = new Swiper('.swiper-thumbs', {
-    speed: 700,
-    spaceBetween: 100,
-    direction: 'horizontal',
-    loop: false,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    breakpoints: {
-        1080:{
-            slidesPerView: 'auto',
-            spaceBetween: 30
-        }
-    },
-    slideToClickedSlide: true
-    // // Navigation arrows
-    // navigation: {
-    //     nextEl: '.right-slide',
-    //     prevEl: '.left-slide',
-    // },
-    
-});
-
-
-
-swiperHistory.on('slideChange', function (swiper) {
-    let swipeIndex = swiperHistory.realIndex;
-    
-    swiperThumbs.activeIndex = swipeIndex
-
-    swiperThumbs.updateSlidesClasses()
-
-    
-});
-
-
-const butInd = document.querySelectorAll('.buttons-date__item');
-    butInd.forEach((btn, i) => {
-        btn.addEventListener('click', () =>{
-            
-            swiperHistory.slideTo(i)
-        })
-    })
-
-
-const swiperWas = new Swiper('.swiper--was', {
-    speed: 700,
-    spaceBetween: 100,
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.right-slide',
-        prevEl: '.left-slide',
-    },
-    breakpoints: {
-        // when window width is >= 320px
-        1080: {
-            slidesPerView: 'auto',
-            spaceBetween: 20
+sliderList.forEach((item, i) =>{
+    const buttonsId = item.getAttribute('data-buttons-id')
+    let options = {
+        speed: 700,
+        spaceBetween: 100,
+        direction: 'horizontal',
+        navigation: {
+            nextEl: `[data-slider-right="${buttonsId}"]`,
+            prevEl: `[data-slider-left="${buttonsId}"]`,
         },
-        // when window width is >= 480px
+        loop: true,
+    }
+    const id = item.getAttribute('data-slider')
+    let isThumbs = item.hasAttribute('data-slider-thumbs')
+    let thumbsSlider
+
+    if (isThumbs) {
+        console.log('done')
+        const thumbsEl = document.querySelector('[data-thumbs]')
+        thumbsSlider = new Swiper(thumbsEl , {
+            ...options,
+            loop: false,
+            breakpoints: {
+                1080:{
+                    slidesPerView: 'auto',
+                    spaceBetween: 30
+                }
+            },
+            
+        })
+    }
+
+    switch (id) {
+        case '1':
+            options = {
+                ...options,
+                loop: false,
+                on: {
+                    slideChange: function() {
+                        let swipeIndex = slider.realIndex;
+                        thumbsSlider.activeIndex = swipeIndex
+                        // console.log(slider.activeIndex)
+                        thumbsSlider.updateSlidesClasses()
+                        
+                    },
+                    init: function(){
+                        const butInd = document.querySelectorAll('.buttons-date__item');
+                        butInd.forEach((btn, i) => {
+                            btn.addEventListener('click', () =>{
+                                console.log(i)
+                                // console.log(slider)
+                                slider.slideTo(i)
+                            })
+                        })
+                    }
+                },
+                
+            }
+            break;
+        case 'text':
+            options = {
+                ...options,
+                speed: 0,
+                spaceBetween: 10,
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+            }
+            break;
         
-    },
-    centeredSlides: true
-});
-
-const swiperPag = new Swiper('.swiper-pag', {
-    speed: 700,
-    spaceBetween: 0,
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.right-slide-pag',
-        prevEl: '.left-slide-pag',
-    },
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     1080: {
-    //         slidesPerView: 'auto',
-    //         spaceBetween: 20
-    //     },
-    //     // when window width is >= 480px
-        
-    // },
-    centeredSlides: true
-});
-
-const swiperPagText = new Swiper('.swiper-pag-text', {
-    speed: 0,
-    spaceBetween: 30,
-    loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination--custom',
-        type: 'bullets',
-        
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.right-slide-pag',
-        prevEl: '.left-slide-pag',
-    },
-    // breakpoints: {
-    //     // when window width is >= 320px
-    //     1080: {
-    //         slidesPerView: 'auto',
-    //         spaceBetween: 20
-    //     },
-    //     // when window width is >= 480px
-        
-    // },
+    }
+    
+    let slider = new Swiper(item, options)
+    
 });
 
 
