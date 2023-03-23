5595ce0a57dcb0e7cb075685081f5d99f595a60f
Gleb <glebshaevvv@mail.ru>
2023-03-23 08:05:25 +0300

    ip

--- src/scripts/counters.js
+++ src/scripts/counters.js
@@ -1,13 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
+  let counter = 5;
   window.addEventListener('click', (event) => {
-    const element = event.target
-    // console.log('done')
-    if (element.classList.contains('counter__button')){
-      console.log('done')
+    const element = event.target;
+    console.log(element)
+
+
+    
+    if (element.closest('[data-counter-left]')){
+      
+      console.log('jop')
+      counter--;
+      // const id = element.dataset.counterLeft;
+      console.log(element.dataset.counterLeft);
+      // document.querySelector(`[data-number-container="${id}"]`).innerHTML = counter;
     }
     
-  })
+    
+  });
 
 
 
-})
+});
